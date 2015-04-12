6dcd26ae34b1a77b673fabbc2b2c1516b8a2a684
Raz <razgoldin@gmail.com>
2015-04-11 20:57:24 -0400

    got show+delete

--- db/seed.js
+++ db/seed.js
@@ -1,8 +1,8 @@
 var sqlite3 = require("sqlite3").verbose();
-var db = new sqlite3.Database("");
+var db = new sqlite3.Database("blog.db");
 //Posts must contain a title, a body, and the ability to have an image, but feel free to add more as you see fit.
 db.run("INSERT INTO posts (title, body, imageUrl) VALUES (?, ?, ?)", "7 crazy things at the GA Happy Hour", "Today at the happy hour bonanza held at GA everything was going well... WHEN all of a sudden whilst playing a strange game of Associations with dry-erase markers, a huge nuclear blast in testing facilities had a strange effect. EXPOSLOSIVELY Everybody got superhero powers. THEN someone accidentally threw up and their vommit turned everyone into Pugs which took selfies and uploaded themselves into Kenneth Parcel's database of pugme-shots. LUCKILY a venerated, LEMON WDI student had also gained the power of time travel which they used to prevent the vomit. WHAT followed, was a joyous outcry in which all GA students rejoiced at being reverted from pugs (the time travel path followed the plot-hole theory of time travel). AS EVERYONE KNOWS now all crimes are no longer a problem and all world problems are resolved with the various super powers of all the GA students attending happy hour. Including, water materialization in which atoms of Hydrogen and oxygen materialize out of the finger tips of one GA hackstar. The hacker was able to initiate a PUT in the file for the LAWS_of_PHYSICS.gf and pour 300-1000 kiloliters of water out of their fingers", "http://blogs.independent.co.uk/wp-content/uploads/2011/10/Misfits.jpg", function(err) {
   if (err) {
-    throw err;
+    console.log(err);
   }
 });
---
+++ server.js
@@ -0,0 +1,82 @@
+var ejs = require("ejs");
+var express = require("express");
+
+var app = express();
+app.set("view_engine", "ejs");
+var port = 3000;
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+var methodOverride = require("method-override");
+app.use(methodOverride("_method"));
+
+var sqlite3 = require("sqlite3").verbose();
+var db = new sqlite3.Database("/Users/Raz/Projects/Blog/db/blog.db");
+//console.log(db.all("SELECT * FROM posts"));
+
+var request = require("request");
+
+app.get("/", function(req, res) {
+  res.redirect("/posts");
+});
+
+app.get("/posts", function(req, res) {
+  db.all("SELECT * FROM posts;", function(err, dataStoredInPosts) {
+    if (err) console.log(err);
+    else {
+      var pTable = dataStoredInPosts;
+      console.log(pTable);
+      res.render("index.ejs", {
+        posts: pTable,
+      });
+    }
+  });
+});
+
+app.get("/post/:id", function(req, res) {
+  var postId = req.params.id;
+  console.log(postId);
+  db.get("SELECT * FROM posts WHERE id = (?);", postId, function(err, dataInPost) {
+    if (err) console.log(err);
+    else {
+      var pData = dataInPost;
+      console.log(pData);
+      res.render("show.ejs", {
+        post: pData,
+      });
+    }
+  });
+});
+
+app.put("/post/:id/edit", function(req, res) {
+  var editID = req.params.id;
+  console.log(editID);
+  db.get("SELECT * FROM posts WHERE id= (?);", editID, function(err, currentData) {
+    if (err) console.log(err);
+    else {
+      var cPost = currentData;
+      res.render("edit.ejs", {
+        post: cPost
+      });
+    }
+  });
+
+});
+
+app.delete("/post/:id", function(req, res) {
+  var postId = req.params.id;
+  console.log(postId);
+  db.get("DELETE FROM posts WHERE ID = (?);", postId, function(err, deleted) {
+    if (err) console.log(err);
+    else {
+      res.redirect("/posts");
+    }
+  });
+});
+
+app.listen(port, function() {
+  console.log("listening on Port" + port);
+});
