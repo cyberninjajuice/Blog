f9439d9d3e4bdca7a3bac0e69ba41d89280145f5
Raz <razgoldin@gmail.com>
2015-04-12 14:33:32 -0400

    finished show page with comment

--- server.js
+++ server.js
@@ -28,9 +28,8 @@ app.get("/posts", function(req, res) {
   db.all("SELECT * FROM posts;", function(err, dataStoredInPosts) {
     if (err) console.log(err);
     else {
-      var pTable = dataStoredInPosts;
-      console.log(pTable);
-      res.render("index.ejs", {//sets data retrieved as "posts"
+      var pTable = dataStoredInPosts; //console.log(pTable);
+      res.render("index.ejs", { //sets data retrieved as "posts"
         posts: pTable,
       });
     }
@@ -42,11 +41,11 @@ app.post("/posts", function(req, res) {
   var textBody = req.body.body;
   var title = req.body.title;
   var imageUrl = req.body.imageUrl;
-  db.run("INSERT INTO posts (title, body, imageUrl) VALUES (?, ?, ?)", title, textBody, imageUrl, function(err, data){
-    if(err) console.log(err);
-    else {//console.log(data);
-    res.redirect("/posts/");
-  }
+  db.run("INSERT INTO posts (title, body, imageUrl) VALUES (?, ?, ?)", title, textBody, imageUrl, function(err, data) {
+    if (err) console.log(err);
+    else { //console.log(data);
+      res.redirect("/posts/");
+    }
   });
 });
 
@@ -62,9 +61,17 @@ app.get("/post/:id", function(req, res) {
     if (err) console.log(err);
     else {
       var pData = dataInPost;
-      //console.log(pData);
-      res.render("show.ejs", {
-        post: pData,
+      console.log(pData);
+      db.all("SELECT * FROM comments WHERE postID=(?);", postId, function(err, dataInComments) {
+        if (err) console.log(err);
+        else {
+          var cData = dataInComments;
+          console.log(cData);
+          res.render("show.ejs", {
+            post: pData,
+            comments: cData
+          });
+        }
       });
     }
   });
@@ -85,10 +92,10 @@ app.get("/post/:id/edit", function(req, res) {
   });
 });
 //adding a comment to a particular article... To do this I will require a simple form which allows someone to post to a given postID. There will then have to be an associated comments table in the same database. When someone writes a comment to a particular post it will have to update that comment's PostID (much like an author's ID would appear on the books description) that comment's body (and title in some blogs but usually not...) that comment's user..It should have an automatically generated timestamp. Once a comment is posted to a particular article IF any comments exists they must be displayed on that post's page (along with the user, title, timestamp). Ideally the homepage where all the articles are would have a link with "0 comments" or more written. By clicking on that page the user is directed to: post/:id/comments/ (Ideally there woulde only one page which would allow editing or deleting of all the comments. This may be achieved by having a textbody editor appear for each comment who's postID matches the req.params.id.) In conclusion comments require the following: (id, tag, body, author, postID-set by looking at id of comments written). The count of comments associated with a specific blogpost may be achieved by comments.withpostID(?).length. 
-app.post("/post/:id/", function(req, res) {
-  var comID=req.params.id;
-  db.run("INSERT INTO comments")
-});
+// app.post("/post/:id/", function(req, res) {
+//   var comID=req.params.id;
+//   db.run("INSERT INTO comments")
+// });
 //upon click on edit this article Source: (index/show) Leads:(Home, Delete, Edit)
 app.put("/post/:id/", function(req, res) {
   var editID = req.params.id;
@@ -101,14 +108,15 @@ app.put("/post/:id/", function(req, res) {
     res.redirect("/post/" + editID);
   });
 });
-app.delete("/post/:id/comment/", function(req,res) {
-  var postID= req.params.id, comID= req.body.id;
-  db.get("DELETE FROM comments WHERE ID = (?);", comID, function(err, deleted){
-    if(err) console.log(err);
-    else{
+app.delete("/post/:id/comment/", function(req, res) {
+  var postID = req.params.id,
+    comID = req.body.id;
+  db.get("DELETE FROM comments WHERE ID = (?);", comID, function(err, deleted) {
+    if (err) console.log(err);
+    else {
       res.redirect("/comments");
     }
-  })
+  });
 });
 //Upon click of delete button of a specific article.
 app.delete("/post/:id", function(req, res) {
