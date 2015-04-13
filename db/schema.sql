17e90f826e96c6bf0864911340bec300b227d6f1
Raz <razgoldin@gmail.com>
2015-04-13 12:28:30 -0400

    fixed up sql a bit more?

--- db/schema.sql
+++ db/schema.sql
@@ -2,4 +2,4 @@ DROP TABLE IF EXISTS posts; CREATE TABLE posts ( id INTEGER PRIMARY KEY AUTOINCR
 CREATE TRIGGER timestamp_update BEFORE UPDATE ON posts BEGIN UPDATE posts SET updated_at = CURRENT_TIMESTAMP WHERE id = new.id; END;
 
 DROP TABLE IF EXISTS comments; CREATE TABLE comments ( id INTEGER PRIMARY KEY AUTOINCREMENT, tag TEXT, body TEXT, author TEXT, postID INTEGER, updated_at REAL);
-CREATE TRIGGER timestamp_update BEFORE UPDATE ON comments BEGIN UPDATE comments SET updated_at = CURRENT_TIMESTAMP WHERE id = new.id; END;
+CREATE TRIGGER c_timestamp_update BEFORE UPDATE ON comments BEGIN UPDATE comments SET updated_at = CURRENT_TIMESTAMP WHERE id = new.id; END;
